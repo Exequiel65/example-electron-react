c2b5f06db1c211379a27bcfde95ac738964187c0
Exequiel65 <marcos97britos@gmail.com>
2025-04-09 18:48:17 -0300

    add example project for electron

---
+++ build/installer.nsh
@@ -0,0 +1,6 @@
+!define MUI_HEADER
+!define MUI_HEADER_BITMAP "build\image\header.bmp"
+!define MUI_HEADER_UNBITMAP "build\image\header.bmp"
+
+!define MUI_SIDEBAR
+!define MUI_SIDEBAR_BITMAP "build\image\sidebar.bmp"
---
+++ electron/main.ts
@@ -0,0 +1,41 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+let mainWindow: BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Cargar el frontend en desarrollo o producción
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
